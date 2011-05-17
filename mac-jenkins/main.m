bbe60987d10ffcf8218d3c96c530a935003b3f8c
Trent Kocurek <t2kocurek@gmail.com>
2011-05-16 19:33:04 -0500

    Initial Commit

---
+++ mac-jenkins/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mac-jenkins
+//
+//  Created by Trent Kocurek on 5/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
